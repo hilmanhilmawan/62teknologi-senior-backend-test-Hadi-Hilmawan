7b23497a34c047f26b7f3de0def721dfdedd08b4
hilman tole <hadi.hilmawan@j-express.id>
2023-06-11 02:49:38 +0700

    first commit

---
+++ src/main/java/com/enamdua/EnamduaApplication.java
@@ -0,0 +1,13 @@
+package com.enamdua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnamduaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnamduaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enamdua/EnamduaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enamdua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnamduaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
